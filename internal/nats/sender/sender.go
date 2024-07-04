a61dc5958fc7ead5d11958601fadcab8b9de7a2f
Maxim Nurmukhametov <maxim.nurmukhametov@gmail.com>
2024-07-04 21:55:15 +0500

    add basic sender

---
+++ internal/nats/sender/sender.go
@@ -0,0 +1,61 @@
+package sender
+
+import (
+	m "github.com/maxnrm/teleflood/pkg/models"
+	tele "gopkg.in/telebot.v3"
+)
+
+type Sender struct {
+	b *tele.Bot
+}
+
+func New(botToken string) (*Sender, error) {
+	b, err := tele.NewBot(tele.Settings{
+		Token: botToken,
+	})
+
+	return &Sender{
+		b: b,
+	}, err
+}
+
+func (s *Sender) Send(fm *m.FloodMessage) error {
+
+	var object tele.Sendable
+
+	switch fm.Type {
+	case m.Text:
+		_, err := s.b.Send(&fm.Recipient, fm.Text, fm.SendOptions)
+		return err
+	case m.Audio:
+		object = fm.Audio
+	case m.Document:
+		object = fm.Document
+	case m.Photo:
+		object = fm.Photo
+	case m.Sticker:
+		object = fm.Sticker
+	case m.Voice:
+		object = fm.Voice
+	case m.VideoNote:
+		object = fm.VideoNote
+	case m.Video:
+		object = fm.Video
+	case m.Animation:
+		object = fm.Animation
+	case m.Location:
+		object = fm.Location
+	case m.Venue:
+		object = fm.Venue
+	case m.Poll:
+		object = fm.Poll
+	case m.Game:
+		object = fm.Game
+	case m.Dice:
+		object = fm.Dice
+	}
+
+	_, err := s.b.Send(&fm.Recipient, object, fm.SendOptions)
+
+	return err
+}

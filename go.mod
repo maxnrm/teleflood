9391bddd202b05e78a76e8be60f945c109940b37
Maxim Nurmukhametov <maxim.nurmukhametov@gmail.com>
2024-08-09 23:51:51 +0500

    update pkg/sender to use uber ratelimiter

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.3
 
 require (
 	github.com/nats-io/nats.go v1.36.0
+	go.uber.org/ratelimit v0.3.1
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
 	gopkg.in/telebot.v3 v3.3.6
 )
@@ -11,6 +12,7 @@ require (
 replace gopkg.in/telebot.v3 v3.3.6 => github.com/maxnrm/telebot v0.0.0
 
 require (
+	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
--- pkg/sender/sender.go
+++ pkg/sender/sender.go
@@ -1,30 +1,29 @@
 package sender
 
 import (
-	"context"
 	"fmt"
 	"time"
 
 	"github.com/maxnrm/teleflood/config"
 	"github.com/maxnrm/teleflood/pkg/message"
-	"golang.org/x/time/rate"
+	"go.uber.org/ratelimit"
 	tele "gopkg.in/telebot.v3"
 )
 
 type UserRateLimiter struct {
 	ChatId      string
-	RateLimiter *rate.Limiter
+	RateLimiter ratelimit.Limiter
 	LastMsgSent time.Time
 }
 
 type Sender struct {
 	b *tele.Bot
 	// TODO: delete items unused for some time
-	globalRateLimiter *rate.Limiter
+	globalRateLimiter ratelimit.Limiter
 	userRateLimiters  map[string]*UserRateLimiter
 }
 
-func New(bot *tele.Bot, gl *rate.Limiter) (*Sender, error) {
+func New(bot *tele.Bot, gl *ratelimit.Limiter) (*Sender, error) {
 	return &Sender{
 		b:                bot,
 		userRateLimiters: make(map[string]*UserRateLimiter),
@@ -32,24 +31,21 @@ func New(bot *tele.Bot, gl *rate.Limiter) (*Sender, error) {
 }
 
 func (s *Sender) Send(to tele.Recipient, fm *message.FloodMessage, so *tele.SendOptions) error {
-
-	ctx := context.Background()
 	chatId := fm.Recipient.Recipient()
 
 	// check if we can send complyting to user ratelimit
 	rl, ok := s.userRateLimiters[chatId]
 	if !ok {
-		limit := rate.Every(time.Second / time.Duration(config.USER_RATE_LIMIT))
 		rl = &UserRateLimiter{
 			ChatId:      chatId,
-			RateLimiter: rate.NewLimiter(limit, config.USER_RATE_LIMIT),
+			RateLimiter: ratelimit.New(config.USER_RATE_LIMIT, ratelimit.WithoutSlack),
 		}
 		s.userRateLimiters[chatId] = rl
 	}
-	rl.RateLimiter.Wait(ctx)
+	rl.RateLimiter.Take()
 
 	// check if we can send complying to global rate limit
-	s.globalRateLimiter.Wait(ctx)
+	s.globalRateLimiter.Take()
 
 	_, err := s.b.Send(to, fm, so)
 	if err != nil {
